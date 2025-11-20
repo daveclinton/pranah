2faf906db077bdd3a04dd0d8898746e4ff72d42d
daveclinton <clintondavid46@gmail.com>
2025-11-20 11:30:12 +0300

    feat: add warm up browser

--- lib/use-social-auth.tsx
+++ lib/use-social-auth.tsx
@@ -1,31 +1,36 @@
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
 import { useSSO } from "@clerk/clerk-expo";
-import { useRouter } from "expo-router";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
 import { useState } from "react";
 import { Alert } from "react-native";
 
+WebBrowser.maybeCompleteAuthSession();
+
 export const useSocialAuth = () => {
+  useWarmUpBrowser();
   const [isLoading, setIsLoading] = useState(false);
   const { startSSOFlow } = useSSO();
-  const router = useRouter();
 
   const handleSocialAuth = async (strategy: "oauth_google") => {
     setIsLoading(true);
     try {
-      const { createdSessionId, setActive } = await startSSOFlow({
+      const result = await startSSOFlow({
         strategy,
+        redirectUrl: AuthSession.makeRedirectUri({ path: 'sso-callback' }),
       });
 
-      if (createdSessionId && setActive) {
-        await setActive({ session: createdSessionId });
-        router.push("/(home)");
+      if (result.createdSessionId && result.setActive) {
+        await result.setActive({ session: result.createdSessionId });
+        return { success: true };
       }
-    } catch (err) {
-      console.log("Error in social auth", err);
-      const provider = strategy === "oauth_google" ? "Google" : "Apple";
-      Alert.alert(
-        "Error",
-        `Failed to sign in with ${provider}. Please try again.`
-      );
+
+      Alert.alert("Error", "Failed to create session. Please try again.");
+      return { success: false, error: "Failed to create session" };
+    } catch (err: any) {
+      console.error(`Error signing in with ${strategy}:`, err);
+      Alert.alert("Sign-In Error", err.message || "Something went wrong.");
+      return { success: false, err };
     } finally {
       setIsLoading(false);
     }
